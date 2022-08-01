939b6f43e36cee6712994ab79534e82cc66bd71e
xlxs4 <orousoultzoglou@gmail.com>
2022-08-01 19:49:56 +0300

    Create Nim code AST at compile time

--- brainfuck/src/brainfuck.nim
+++ brainfuck/src/brainfuck.nim
@@ -1,3 +1,11 @@
+when not defined(nimnode):
+  type NimNode = PNimrodNode
+
+{.push overflowchecks: off.}
+proc xinc(c: var char) = inc c
+proc xdec(c: var char) = dec c
+{.pop.}
+
 proc interpret*(code: string) =
   ## Interprets the brainfuck `code` string,
   ## reading from stdin and writing to stdout.
@@ -7,11 +15,6 @@ proc interpret*(code: string) =
     tapePos = 0
 
   proc run(skip = false): bool =
-    {.push overflowchecks: off.}
-    proc xinc(c: var char) = inc c
-    proc xdec(c: var char) = dec c
-    {.pop.}
-
     while tapePos >= 0 and codePos < code.len:
       if tapePos >= tape.len:
         tape.add '\0'
@@ -36,12 +39,54 @@ proc interpret*(code: string) =
 
   discard run()
 
-when isMainModule:
-  import os
+import macros
 
+proc compile(code: string): NimNode {.compiletime.} =
+  var stmts = @[newStmtList()]
+
+  template addStmt(text): typed =
+    stmts[stmts.high].add parseStmt(text)
+
+  addStmt "var tape: array[1_000_000, char]"
+  addStmt "var codePos = 0"
+  addStmt "var tapePos = 0"
+
+  for c in code:
+    case c
+    of '+': addStmt "xinc tape[tapePos]"
+    of '-': addStmt "xdec tape[tapePos]"
+    of '>': addStmt "inc tapePos"
+    of '<': addStmt "dec tapePos"
+    of '.': addStmt "stdout.write tape[tapePos]"
+    of ',': addStmt "tape[tapePos] = stdin.readChar"
+    of '[': stmts.add newStmtList()
+    of ']':
+      var loop = newNimNode(nnkWhileStmt)
+      loop.add parseExpr("tape[tapePos] != '\\0'")
+      loop.add stmts.pop
+      stmts[stmts.high].add loop
+    else: discard
+
+  result = stmts[0]
+
+macro compileString*(code: string): typed =
+  ## Compiles the brainfuck `code` string into Nim code
+  ## that reads from stdin and writes to stdout.
+  compile code.strVal
+
+macro compileFile*(filename: string): typed =
+  ## Compiles the brainfuck code read from `filename` at compile time
+  ## into Nim code that reads from stdin 
+  compile staticRead(filename.strVal)
+
+when isMainModule:
   echo "Welcome to brainfuck"
 
-  let code = if paramCount() > 0: readFile paramStr(1)
-             else: readAll stdin
+  proc mandelbrot = compileFile "../examples/mandelbrot.b"
+  mandelbrot()
 
-  interpret code
+  # import os
+  # let code = if paramCount() > 0: readFile paramStr(1)
+  #            else: readAll stdin
+
+  # interpret code
