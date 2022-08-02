c31b6e31b7cca4100296777d58f95367aafe12c1
xlxs4 <orousoultzoglou@gmail.com>
2022-08-02 13:23:38 +0300

    Refactor to choose IO streams at will

--- brainfuck/src/brainfuck.nim
+++ brainfuck/src/brainfuck.nim
@@ -1,12 +1,19 @@
+import streams
+
 when not defined(nimnode):
   type NimNode = PNimrodNode
+
+proc readCharEOF*(input: Stream): char =
+  result = input.readChar
+  if result == '\0': # Streams return 0 for EOF
+    result = '\255'  # BF assumes EOF to be -1
 
 {.push overflowchecks: off.}
 proc xinc(c: var char) = inc c
 proc xdec(c: var char) = dec c
 {.pop.}
 
-proc interpret*(code: string) =
+proc interpret*(code: string; input, output: Stream) =
   ## Interprets the brainfuck `code` string,
   ## reading from stdin and writing to stdout.
   var
@@ -18,34 +25,58 @@ proc interpret*(code: string) =
     while tapePos >= 0 and codePos < code.len:
       if tapePos >= tape.len:
         tape.add '\0'
+
       if code[codePos] == '[':
         inc codePos
         let oldPos = codePos
         while run(tape[tapePos] == '\0'):
           codePos = oldPos
+
       elif code[codePos] == ']':
         return tape[tapePos] != '\0'
+
       elif not skip:
         case code[codePos]
         of '+': xinc tape[tapePos]
         of '-': xdec tape[tapePos]
         of '>': inc tapePos
         of '<': dec tapePos
-        of '.': stdout.write tape[tapePos]
-        of ',': tape[tapePos] = stdin.readChar
+        of '.': output.write tape[tapePos]
+        of ',': tape[tapePos] = input.readCharEOF
         else: discard
 
       inc codePos
 
   discard run()
 
+proc interpret*(code, input: string): string =
+  ## Interprets the brainfuck `code` string,
+  ## reading from `input` and returning the result directly.
+  var outStream = newStringStream()
+  interpret(code, input.newStringStream, outStream)
+  result = outStream.data
+
+proc interpret*(code: string) =
+  ## Interprets the brainfuck `code` string,
+  ## reading from stdin and writing to stdout
+  interpret(code, stdin.newFileStream, stdout.newFileStream)
+
 import macros
 
-proc compile(code: string): NimNode {.compiletime.} =
+proc compile(code, input, output: string): NimNode {.compiletime.} =
   var stmts = @[newStmtList()]
 
   template addStmt(text): typed =
     stmts[stmts.high].add parseStmt(text)
+
+  addStmt """
+  when not compiles(newStringStream()):
+    static:
+      quit("Error: Import the streams module to compile brainfuck code", 1)
+  """
+
+  addStmt "var inpStream = " & input
+  addStmt "var outStream = " & output
 
   addStmt "var tape: array[1_000_000, char]"
   addStmt "var tapePos = 0"
@@ -56,8 +87,8 @@ proc compile(code: string): NimNode {.compiletime.} =
     of '-': addStmt "xdec tape[tapePos]"
     of '>': addStmt "inc tapePos"
     of '<': addStmt "dec tapePos"
-    of '.': addStmt "stdout.write tape[tapePos]"
-    of ',': addStmt "tape[tapePos] = stdin.readChar"
+    of '.': addStmt "outStream.write tape[tapePos]"
+    of ',': addStmt "tape[tapePos] = inpStream.readCharEOF"
     of '[': stmts.add newStmtList()
     of ']':
       var loop = newNimNode(nnkWhileStmt)
@@ -68,15 +99,32 @@ proc compile(code: string): NimNode {.compiletime.} =
 
   result = stmts[0]
 
+macro compileString*(code: string; input, output: untyped): typed =
+  ## Compiles the brainfuck code read from `filename` at compile time
+  ## into Nim code that reads from the `input` variable and
+  ## writes to the `output` variable, both strings.
+  result = compile($code,
+    "newStringStream(" & $input & ")", "newStringStream()")
+  result.add parseStmt($output & " = outStream.data")
+
 macro compileString*(code: string): typed =
   ## Compiles the brainfuck `code` string into Nim code
   ## that reads from stdin and writes to stdout.
-  compile code.strVal
+  compile($code, "stdin.newFileStream", "stdout.newFileStream")
+
+macro compileFile*(filename: string; input, output: untyped): typed =
+  ## Compiles the brainfuck code read from `filename` at compile time
+  ## into Nim code that reads from the `input` variable
+  ## and writes to the `output` variable, both strings.
+  result = compile(staticRead(filename.strVal),
+    "newStringStream(" & $input & ")", "newStringStream()")
+  result.add parseStmt($output & " = outStream.data")
 
 macro compileFile*(filename: string): typed =
   ## Compiles the brainfuck code read from `filename` at compile time
-  ## into Nim code that reads from stdin 
-  compile staticRead(filename.strVal)
+  ## into Nim code that reads from stdin and writes to stdout.
+  compile(staticRead(filename.strVal),
+          "stdin.newFileStream", "stdout.newFileStream")
 
 when isMainModule:
   import docopt, tables
