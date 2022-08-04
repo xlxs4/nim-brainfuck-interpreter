ae103f06ba32b50e51b271b86ab5edc02a7a2e43
xlxs4 <orousoultzoglou@gmail.com>
2022-08-04 17:52:53 +0300

    Add introduction in docs

--- src/brainfuck.nim
+++ src/brainfuck.nim
@@ -7,7 +7,7 @@
 ## 
 ## This is a toy interpreter for the [brainfuck](https://www.wikiwand.com/en/Brainfuck)
 ## programming language written fully in Nim.
-## It doubles as a compiler of brainfuck into efficient Nim code.
+## It doubles as a [transpiler](https://www.wikiwand.com/en/Source-to-source_compiler) of brainfuck into efficient Nim code.
 ## 
 ## ----
 ##
@@ -21,6 +21,50 @@
 ##
 ##   proc mandelbrot = compileFile("examples/mandelbrot.b")
 ##   mandelbrot() # Draws a mandelbrot set in ASCII
+## 
+## ## Introduction
+##
+## Brainfuck is a very small esoteric programming language.
+## It's one of the most famous esoteric languages and has inspired many esoteric programming language creators.
+## 
+## Brainfuck only consists of eight simple commands, a data pointer and an instruction pointer (program counter).
+## The commands are the following:
+## 
+## - `>`: Increment the data pointer (to point to the next cell to the right)
+## - `<`: Decrement the data pointer (to point to the next cell to the left)
+## - `+`: Increment the byte at the data pointer
+## - `-`: Decrement the byte at the data pointer
+## - `.`: Output the byte at the data pointer
+## - `,`: Accept one byte of input, storing its value in the byte at the data pointer
+## - `[`: If the byte at the data pointer is zero, then instead of moving the instruction pointer forward to the next command, jump it *forward* to the command after the *matching* `]` command
+## - `]`: If the byte at the data pointer is *non*zero, then instead of moving the instruction pointer forward to the next command, jump it *back* to the command after the *matching* `[` command
+## 
+## ## Usage
+## 
+## Usage:
+## 
+## .. code::
+##   brainfuck mandelbrot
+##   brainfuck interpret [<file.b>]
+##   brainfuck (-h | --help)
+##   brainfuck (-v | --version)
+## 
+## Options:
+## 
+## .. code::
+##   -h --help     Show this screen.
+##   -v --version  Show version.
+## 
+## ## Building
+##
+## 1. install [nim](https://nim-lang.org/)
+## 2. `git clone https://github.com/xlxs4/nim-brainfuck-interpreter.git`
+## 3. `cd nim-brainfuck-interpreter/`
+## 4. `nimble build`
+##
+## After that you will get a ready-made binary file in the root directory of the project.
+##
+
 
 import streams
 
